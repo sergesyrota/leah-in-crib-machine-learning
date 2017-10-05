a639fd860462b30a6e131f7668b29ff4d446f127
Sergey Syrota <serge.syrota@gmail.com>
2017-10-05 08:38:00 -0500

    Updating get_dataset to be more universal for all labeled images

--- get_dataset.py
+++ get_dataset.py
@@ -6,62 +6,49 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-def get_dataset(sess):
-    image_base='/Users/sergeysyrota/git-source/tensorflow/leah-in-crib/images/'
-
+def get_dataset(count, image_base, max_delta = 0.04, return_original = False, desired_shape = (160*90, 1), append_axis = 1):
     # NO images
-    no_image_path=tf.constant(image_base + 'no/tmp.*')
-    no_image_files = tf.matching_files(no_image_path)
-    no_filename_queue = tf.train.string_input_producer(no_image_files)
+    image_path = [
+        tf.constant(image_base + 'no/tmp.*'),
+        tf.constant(image_base + 'yes/tmp.*'),
+        tf.constant(image_base + 'no/homebot-*'),
+        tf.constant(image_base + 'yes/homebot-*')
+    ]
+    image_files = tf.matching_files(image_path)
+    filename_queue = tf.train.string_input_producer(image_files, seed=1)
     reader = tf.WholeFileReader()
-    no_key, no_value = reader.read(no_filename_queue)
-    no_one_image = tf.to_float(tf.image.decode_jpeg(no_value, channels=1, ratio=8))
-    # YES images
-    yes_image_path=tf.constant(image_base + 'yes/tmp.*')
-    yes_image_files = tf.matching_files(yes_image_path)
-    yes_filename_queue = tf.train.string_input_producer(yes_image_files)
-    reader = tf.WholeFileReader()
-    yes_key, yes_value = reader.read(yes_filename_queue)
-    yes_one_image = tf.to_float(tf.image.decode_jpeg(yes_value, channels=1, ratio=8))
+    file_name, file_content = reader.read(filename_queue)
+    one_image = tf.image.decode_jpeg(file_content, channels=1, ratio=8)
+    #resized_image = tf.image.resize_images(one_image, [5,5])
+    adjusted_image = tf.image.random_brightness(one_image, max_delta, seed=1)
 
+    sess = tf.Session()
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    train_images = None
-    train_labels = None
-    eval_images = None
-    eval_labels = None
-    np.random.seed(8579)  # Fixed seed, so the same images fall into train and eval sets on consecutive runs
-    # no images
-    for i in range(30):
-        flat_image = sess.run(no_one_image).reshape((1, 90, 160, 1))
-        if (train_images is None):
-            train_images = flat_image
-            train_labels = [0]
-        elif (eval_images is None):
-            eval_images = flat_image
-            eval_labels = [1]
-        # randomly distribute between train and eval sets, but not really, as we are using a constant seed
-        elif (np.random.randint(0, 10) < 1):
-            eval_images = np.append(eval_images, flat_image, 0)
-            eval_labels = np.append(eval_labels, [0], 0)
+    images = None
+    labels = None
+    originals = []
+    for i in range(count):
+        image = sess.run({
+            "file_name": file_name,
+            "original_image": one_image,
+            "adjusted_image": adjusted_image
+        })
+        if '/yes/' in image['file_name'].decode('ascii'):
+            image["label"] = 1
         else:
-            train_images = np.append(train_images, flat_image, 0)
-            train_labels = np.append(train_labels, [0], 0)
-        #print(sess.run(one_image).reshape((90*160,1)).shape)
-    # yes images
-    for i in range(30):
-        flat_image = sess.run(yes_one_image).reshape((1,90,160,1))
-        if (np.random.randint(0,10) < 1):
-            eval_images = np.append(eval_images, flat_image, 0)
-            eval_labels = np.append(eval_labels, [1], 0)
+            image["label"] = 0
+        if images is None:
+            images = image["adjusted_image"].reshape(desired_shape)
+            labels = [[image["label"]]]
         else:
-            train_images = np.append(train_images, flat_image, 0)
-            train_labels = np.append(train_labels, [1], 0)
-        #print(sess.run(one_image).reshape((90*160,1)).shape)
-    #print(train_images, train_labels)
-    #print(eval_images, eval_labels)
-    print(train_labels.shape)
-    print(eval_labels.shape)
+            images = np.append(images, image["adjusted_image"].reshape(desired_shape), append_axis)
+            labels = np.append(labels, [[image["label"]]], append_axis)
+        if return_original:
+            originals.append(image)
+        #print(image['file_name'])
+        #print("original", image["original_image"][0:5,0:5])
+        #print("adjusted", image["adjusted_image"][0:5,0:5])
     coord.request_stop()
     coord.join(threads)
-    return (train_images, train_labels, eval_images, eval_labels)
+    return (np.float32(images/255), labels, originals)
